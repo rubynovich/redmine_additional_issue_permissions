371df05f20498c7f3d5fa05bb622ed52d8740972
rubynovich <roman@shipiev.me>
2013-03-19 17:34:16 +0400

    fix: link_to_remote -> link_to

--- lib/aip_issue_patch.rb
+++ lib/aip_issue_patch.rb
@@ -4,16 +4,16 @@ module AdditionalIssuePermissionsPlugin
   module IssuePatch
     def self.included(base)
       base.extend(ClassMethods)
-      
+
       base.send(:include, InstanceMethods)
-      
+
       base.class_eval do
         validates_presence_of :assigned_to_id, :due_date
-              
-        safe_attributes 'status_id', 
+
+        safe_attributes 'status_id',
           :if => lambda {|issue, user|
             (
-              (user == issue.assigned_to) && 
+              (user == issue.assigned_to) &&
                 user.allowed_to?(:edit_status_4assigned_to, issue.project)
             ) || (
               (user == issue.author) &&
@@ -22,9 +22,9 @@ module AdditionalIssuePermissionsPlugin
               user.allowed_to?(:edit_status, issue.project)
           }
         safe_attributes 'assigned_to_id',
-          :if => lambda {|issue, user| 
+          :if => lambda {|issue, user|
             (
-              (user == issue.assigned_to) && 
+              (user == issue.assigned_to) &&
                 user.allowed_to?(:edit_assigned_to_4assigned_to, issue.project)
             ) || (
               (user == issue.author) &&
@@ -35,7 +35,7 @@ module AdditionalIssuePermissionsPlugin
         safe_attributes 'done_ratio',
           :if => lambda {|issue, user|
             (
-              (user == issue.assigned_to) && 
+              (user == issue.assigned_to) &&
                 user.allowed_to?(:edit_done_ratio_4assigned_to, issue.project)
             ) || (
               (user == issue.author) &&
@@ -46,7 +46,7 @@ module AdditionalIssuePermissionsPlugin
         safe_attributes 'start_date',
           :if => lambda {|issue, user|
             (
-              (user == issue.assigned_to) && 
+              (user == issue.assigned_to) &&
                 user.allowed_to?(:edit_start_date_4assigned_to, issue.project)
             ) || (
               (user == issue.author) &&
@@ -55,9 +55,9 @@ module AdditionalIssuePermissionsPlugin
               user.allowed_to?(:edit_start_date, issue.project)
           }
         safe_attributes 'due_date',
-          :if => lambda {|issue, user| 
+          :if => lambda {|issue, user|
             (
-              (user == issue.assigned_to) && 
+              (user == issue.assigned_to) &&
                 user.allowed_to?(:edit_due_date_4assigned_to, issue.project)
             ) || (
               (user == issue.author) &&
@@ -68,7 +68,7 @@ module AdditionalIssuePermissionsPlugin
         safe_attributes 'parent_issue_id',
           :if => lambda {|issue, user|
             (
-              (user == issue.assigned_to) && 
+              (user == issue.assigned_to) &&
                 user.allowed_to?(:edit_parent_issue_4assigned_to, issue.project)
             ) || (
               (user == issue.author) &&
@@ -76,23 +76,23 @@ module AdditionalIssuePermissionsPlugin
             ) ||
               user.allowed_to?(:edit_parent_issue, issue.project)
           }
-        safe_attributes 'estimated_hours', 
-          :if => lambda {|issue, user| 
+        safe_attributes 'estimated_hours',
+          :if => lambda {|issue, user|
             (
-              (user == issue.assigned_to) && 
+              (user == issue.assigned_to) &&
                 user.allowed_to?(:edit_estimated_hours_4assigned_to, issue.project)
             ) || (
               (user == issue.author) &&
                 user.allowed_to?(:edit_estimated_hours_4author, issue.project)
             ) ||
-              user.allowed_to?(:edit_estimated_hours, issue.project)        
+              user.allowed_to?(:edit_estimated_hours, issue.project)
           }
       end
     end
-      
+
     module ClassMethods
     end
-    
+
     module InstanceMethods
       def allowed_additional_premissions?(user=User.current)
         %w{status assigned_to done_ratio start_date due_date parent_issue estimated_hours}.any?{ |field|
@@ -105,38 +105,38 @@ module AdditionalIssuePermissionsPlugin
           ) || user.allowed_to?(:"edit_#{field}", self.project)
         }
       end
-      
+
       def status_allowed?(user=User.current)
         if user == self.author
           user.allowed_to?(:edit_status_4author, self.project)
         elsif user == self.assigned_to
           user.allowed_to?(:edit_status_4assigned_to, self.project)
         else
-          self.new_record? || 
+          self.new_record? ||
             user.allowed_to?(:edit_issues, self.project) ||
             user.allowed_to?(:edit_status, self.project)
         end
       end
-      
+
       def assigned_to_allowed?(user=User.current)
         if user == self.author
           user.allowed_to?(:edit_assigned_to_4author, self.project)
         elsif user == self.assigned_to
           user.allowed_to?(:edit_assigned_to_4assigned_to, self.project)
         else
-          self.new_record? || 
+          self.new_record? ||
             user.allowed_to?(:edit_issues, self.project) ||
             user.allowed_to?(:edit_assigned_to, self.project)
         end
       end
-      
+
       def done_ratio_allowed?(user=User.current)
         if user == self.author
           user.allowed_to?(:edit_done_ratio_4author, self.project)
         elsif user == self.assigned_to
           user.allowed_to?(:edit_done_ratio_4assigned_to, self.project)
         else
-          self.new_record? || 
+          self.new_record? ||
             user.allowed_to?(:edit_issues, self.project) ||
             user.allowed_to?(:edit_done_ratio, self.project)
         end
